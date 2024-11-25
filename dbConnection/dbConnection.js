e9fe0d7d86acc9d2ec7710f9a04274e8a866ef48
Fiza <131092372+Fizahere@users.noreply.github.com>
2024-11-25 14:44:27 +0500

    [folder structure]

---
+++ dbConnection/dbConnection.js
@@ -0,0 +1,12 @@
+const { connect } = require("mongoose");
+
+ const connectDb = async () => {
+    try {
+        await connect('');
+        console.log('connected.');
+    } catch (error) {
+        console.log("can't cannect.", error.message);
+    }
+}
+
+export default connectDb;
--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 import express from 'express';
+import connectDb from './dbConnection/dbConnection';
 
-const app=express();
-const port=2000;
-app.use(express.json())
+const app = express();
+const port = 2000;
+connectDb();
+app.use(express.json());
+// app.use('/api')
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`server is running on port ${port}`)
 });
