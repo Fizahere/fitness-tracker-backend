9dcc6fb80470b201d9e83eb9dc4283a14a549c37
Fiza <131092372+Fizahere@users.noreply.github.com>
2024-12-08 18:52:07 +0500

    [nutrition by id]

--- routes/nutritionRoutes.js
+++ routes/nutritionRoutes.js
@@ -2,18 +2,20 @@ import { Router } from "express";
 import {
     addNutritions,
     deleteNutritions,
+    getNutritionById,
     getNutritions,
     searchNutritions,
     updateNutritions
-}from '../Controllers/NutritionController.js';
+} from '../Controllers/NutritionController.js';
 import { authenticateToken } from "../Middlewares/authMiddleWare.js";
 
 const nutritionRouter = Router()
 
-nutritionRouter.get('/get-nutritions',authenticateToken, getNutritions);
+nutritionRouter.get('/get-nutritions', authenticateToken, getNutritions);
+nutritionRouter.get('/get-nutrition-by-id/:id', getNutritionById)
 nutritionRouter.get('/search-nutrition/:searchQuery', searchNutritions);
-nutritionRouter.post('/add-nutrition',authenticateToken, addNutritions);
+nutritionRouter.post('/add-nutrition', authenticateToken, addNutritions);
 nutritionRouter.put('/update-nutritions/:id', updateNutritions)
-nutritionRouter.delete('/delete-nutrition/:id',deleteNutritions)
+nutritionRouter.delete('/delete-nutrition/:id', deleteNutritions)
 
 export default nutritionRouter;
